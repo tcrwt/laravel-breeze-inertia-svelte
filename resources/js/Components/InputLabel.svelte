5728bee7fdfdd081287990ec6783299538c1d676
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-16 21:36:53 +0100

    Create InputLabel.svelte

---
+++ resources/js/Components/InputLabel.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let value: string = '';
+</script>
+
+<label {...$$restProps} class="block font-medium text-sm text-gray-700">
+    {#if value}
+        <span>{value}</span>
+    {:else}
+        <span><slot /></span>
+    {/if}
+</label>
+
