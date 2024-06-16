72f4a615949c50446add07f3f635f63b18f79d51
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-16 20:54:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
     plugins: [
@@ -15,6 +16,11 @@ export default defineConfig({
                     base: null,
                     includeAbsolute: false,
                 },
+            },
+        }),
+        svelte({
+            compilerOptions: {
+                hydratable: true,
             },
         }),
     ],
