8232ce8f17a77fd647cd639010a89876c1e46314
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-16 21:33:31 +0100

    Create InputError.svelte

---
+++ resources/js/Components/InputError.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let message: string = '';
+</script>
+
+{#if message}
+    <div>
+        <p class="text-sm text-red-600">
+            {message}
+        </p>
+    </div>
+{/if}
