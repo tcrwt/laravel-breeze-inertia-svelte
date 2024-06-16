0087276c41eff6b45c86a095aab13e80ad4de223
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-16 21:55:54 +0100

    Create TextInput.svelte

---
+++ resources/js/Components/TextInput.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let value: string = '';
+</script>
+
+<input
+    {...$$restProps}
+    class="{$$props.class || ''} border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+    bind:value
+/>
