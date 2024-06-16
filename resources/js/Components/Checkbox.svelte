e28a29ea61586a8d7cb8507a8b32b8f0c711e03b
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-16 21:24:32 +0100

    Create Checkbox.svelte

---
+++ resources/js/Components/Checkbox.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let value: any = undefined;
+
+</script>
+
+<input
+    {...$$restProps}
+    type="checkbox"
+    bind:value
+    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+/>
+
