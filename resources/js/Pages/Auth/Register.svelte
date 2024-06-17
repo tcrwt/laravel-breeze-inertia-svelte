4385afeb60c9bae02a272cbf7147a971b7cbbf3d
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-17 06:46:37 +0100

    :href

--- resources/js/Pages/Auth/Register.svelte
+++ resources/js/Pages/Auth/Register.svelte
@@ -91,7 +91,7 @@ const submit = () => {
 
         <div class="flex items-center justify-end mt-4">
             <Link
-                :href="route('login')"
+                href={route('login')}
                 class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 Already registered?
