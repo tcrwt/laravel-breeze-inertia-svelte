a3279e65ba415ecec29204c9caf3674bbfa8909b
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-18 06:31:26 +0100

    Update NavLink.svelte

--- resources/js/Components/NavLink.svelte
+++ resources/js/Components/NavLink.svelte
@@ -1,21 +1,14 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { Link } from '@inertiajs/vue3';
+    import { Link } from '@inertiajs/svelte';
 
-const props = defineProps<{
-    href: string;
-    active?: boolean;
-}>();
+    export let href: string;
+    export let active: boolean = false;
 
-const classes = computed(() =>
-    props.active
+    $: classes = active
         ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
-);
+        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 </script>
 
-<template>
-    <Link :href="href" :class="classes">
-        <slot />
-    </Link>
-</template>
+<Link {href} class={classes}>
+    <slot />
+</Link>
