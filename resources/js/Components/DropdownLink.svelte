497481caadac4773aa7552c84bd99991c7f230cf
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-18 06:36:22 +0100

    prettier

--- resources/js/Components/DropdownLink.svelte
+++ resources/js/Components/DropdownLink.svelte
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/svelte';
+    import { Link } from '@inertiajs/svelte';
 
-export let href: string;
+    export let href: string;
 </script>
 
-
-    <Link
+<Link
     {...$$restProps}
-        {href}
-        class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
-    >
-        <slot />
-    </Link>
+    {href}
+    class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+>
+    <slot />
+</Link>
