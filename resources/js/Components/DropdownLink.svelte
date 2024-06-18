2809f61ef0ccb7a28f61f4d9d2114118b2d6d788
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-18 06:36:10 +0100

    Update DropdownLink.svelte

--- resources/js/Components/DropdownLink.svelte
+++ resources/js/Components/DropdownLink.svelte
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/svelte';
 
-defineProps<{
-    href: string;
-}>();
+export let href: string;
 </script>
 
-<template>
+
     <Link
-        :href="href"
+    {...$$restProps}
+        {href}
         class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
     >
         <slot />
     </Link>
-</template>
