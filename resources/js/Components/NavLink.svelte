07f37d792fdab4a7568b34e55ffde07c72baee72
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-18 06:35:41 +0100

    restProps! fair play vue

--- resources/js/Components/NavLink.svelte
+++ resources/js/Components/NavLink.svelte
@@ -9,6 +9,6 @@
         : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 </script>
 
-<Link {href} class={classes}>
+<Link {...$$restProps} {href} class={classes}>
     <slot />
 </Link>
