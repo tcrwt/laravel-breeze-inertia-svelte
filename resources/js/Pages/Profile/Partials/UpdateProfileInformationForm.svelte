e08688c7cf6b21f483f923f445f9d936461e6107
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-19 20:06:19 +0100

    prettier and $form fixes

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.svelte
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.svelte
@@ -17,81 +17,79 @@
     });
 </script>
 
+<section {...$$restProps}>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
 
-    <section>
-        <header>
-            <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
+        <p class="mt-1 text-sm text-gray-600">
+            Update your account's profile information and email address.
+        </p>
+    </header>
 
-            <p class="mt-1 text-sm text-gray-600">
-                Update your account's profile information and email address.
-            </p>
-        </header>
+    <form
+        on:submit|preventDefault={$form.patch(route('profile.update'))}
+        class="mt-6 space-y-6"
+    >
+        <div>
+            <InputLabel for="name" value="Name" />
 
-        <form
-            on:submit|preventDefault={$form.patch(route('profile.update'))}
-            class="mt-6 space-y-6"
-        >
+            <TextInput
+                id="name"
+                type="text"
+                class="mt-1 block w-full"
+                bind:value={$form.name}
+                required
+                autofocus
+                autocomplete="name"
+            />
+
+            <InputError class="mt-2" message={$form.errors.name} />
+        </div>
+
+        <div>
+            <InputLabel for="email" value="Email" />
+
+            <TextInput
+                id="email"
+                type="email"
+                class="mt-1 block w-full"
+                bind:value={$form.email}
+                required
+                autocomplete="username"
+            />
+
+            <InputError class="mt-2" message={$form.errors.email} />
+        </div>
+
+        {#if mustVerifyEmail && user.email_verified_at === null}
             <div>
-                <InputLabel for="name" value="Name" />
-
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
-
-                <InputError class="mt-2" message={form.errors.name} />
-            </div>
-
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" message={form.errors.email} />
-            </div>
-
-            {#if mustVerifyEmail && user.email_verified_at === null}
-                <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        Your email address is unverified.
-                        <Link
-                            href={route('verification.send')}
-                            method="post"
-                            as="button"
-                            class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Click here to re-send the verification email.
-                        </Link>
-                    </p>
-
-                    <div
-                        hidden={status !== 'verification-link-sent'}
-                        class="mt-2 font-medium text-sm text-green-600"
+                <p class="text-sm mt-2 text-gray-800">
+                    Your email address is unverified.
+                    <Link
+                        href={route('verification.send')}
+                        method="post"
+                        as="button"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                        A new verification link has been sent to your email address.
-                    </div>
+                        Click here to re-send the verification email.
+                    </Link>
+                </p>
+
+                <div
+                    hidden={status !== 'verification-link-sent'}
+                    class="mt-2 font-medium text-sm text-green-600"
+                >
+                    A new verification link has been sent to your email address.
                 </div>
-            {/if}
-
-            <div class="flex items-center gap-4">
-                <PrimaryButton disabled={$form.processing}>Save</PrimaryButton>
-
-                {#if $form.recentlySuccessful}
-                    <p transition:fade={{}} class="text-sm text-gray-600">Saved.</p>
-                {/if}
             </div>
-        </form>
-    </section>
+        {/if}
 
+        <div class="flex items-center gap-4">
+            <PrimaryButton disabled={$form.processing}>Save</PrimaryButton>
+
+            {#if $form.recentlySuccessful}
+                <p transition:fade class="text-sm text-gray-600">Saved.</p>
+            {/if}
+        </div>
+    </form>
+</section>
