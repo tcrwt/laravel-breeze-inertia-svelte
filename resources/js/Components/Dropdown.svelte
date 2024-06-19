105eaaa38f1238a13bc65988e5043f7cc612d278
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-19 05:57:18 +0100

    prettier

--- resources/js/Components/Dropdown.svelte
+++ resources/js/Components/Dropdown.svelte
@@ -1,56 +1,55 @@
 <script setup lang="ts">
     import { onMount } from 'svelte';
 
+    export let align: 'left' | 'right' = 'right';
+    // TODO: ... width as string :what:
+    export let width: '48' = '48';
+    export let contentClasses: string = 'py-1 bg-white';
 
-export let align: 'left' | 'right' = 'right';
-// TODO: ... width as string :what:
-export let width: '48' = '48';
-export let contentClasses: string = 'py-1 bg-white';
-
-
-const closeOnEscape = (e: KeyboardEvent) => {
-    if (open.value && e.key === 'Escape') {
-        open.value = false;
-    }
-};
-
-onMount(()=>{
-    document.addEventListener('keydown', closeOnEscape);
-
-    return () => {
-        document.removeEventListener('keydown', closeOnEscape);
+    const closeOnEscape = (e: KeyboardEvent) => {
+        if (open.value && e.key === 'Escape') {
+            open.value = false;
+        }
     };
-})
 
-$: widthClass = {48: 'w-48'}[width.toString()];
+    onMount(() => {
+        document.addEventListener('keydown', closeOnEscape);
 
-let alignmentClasses: string;
-$: {
-    if (align === 'left') {
-        alignmentClasses= 'ltr:origin-top-left rtl:origin-top-right start-0';
-    } else if (align === 'right') {
-        alignmentClasses= 'ltr:origin-top-right rtl:origin-top-left end-0';
-    } else {
-        alignmentClasses= 'origin-top';
+        return () => {
+            document.removeEventListener('keydown', closeOnEscape);
+        };
+    });
+
+    $: widthClass = { 48: 'w-48' }[width.toString()];
+
+    let alignmentClasses: string;
+    $: {
+        if (align === 'left') {
+            alignmentClasses = 'ltr:origin-top-left rtl:origin-top-right start-0';
+        } else if (align === 'right') {
+            alignmentClasses = 'ltr:origin-top-right rtl:origin-top-left end-0';
+        } else {
+            alignmentClasses = 'origin-top';
+        }
     }
-};
 
-let open = false;
+    let open = false;
 </script>
 
+<div class="relative">
+    <div
+        on:click={() => {
+            open = !open;
+        }}
+    >
+        <slot name="trigger" />
+    </div>
 
-    <div class="relative">
-        <div on:click={()=>{open = !open}}>
-            <slot name="trigger" />
-        </div>
+    <!-- Full Screen Dropdown Overlay -->
 
-        <!-- Full Screen Dropdown Overlay -->
+    <div hidden={!open} class="fixed inset-0 z-40" on:click={() => (open = false)}></div>
 
-        <div 
-        hidden={!open}
-        class="fixed inset-0 z-40" on:click={()=>open = false}></div>
-
-        <!-- <Transition
+    <!-- <Transition
             enter-active-class="transition ease-out duration-200"
             enter-from-class="opacity-0 scale-95"
             enter-to-class="opacity-100 scale-100"
@@ -58,15 +57,14 @@ let open = false;
             leave-from-class="opacity-100 scale-100"
             leave-to-class="opacity-0 scale-95"
         > -->
-            <div
-                class="absolute z-50 mt-2 rounded-md shadow-lg {widthClass} {alignmentClasses}"
-                hidden={!open}
-                on:click={()=>open = false}
-            >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
-                    <slot name="content" />
-                </div>
-            </div>
-        <!-- </Transition> -->
+    <div
+        class="absolute z-50 mt-2 rounded-md shadow-lg {widthClass} {alignmentClasses}"
+        hidden={!open}
+        on:click={() => (open = false)}
+    >
+        <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
+            <slot name="content" />
+        </div>
     </div>
-
+    <!-- </Transition> -->
+</div>
