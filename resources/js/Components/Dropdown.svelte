a3c9b0c7b7bc645d510b8d61b77757ba35e6c1ef
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-19 06:33:51 +0100

    animations
    not perfectly the same – vue version scales to the centre but this seems to scale from top. Might be due to the constraint that svelte transitions happen on add/remove from DOM but vue just seems to hide using v-show.
    I tried using that but the `hidden` behaved differently – I think not possible to animate hidden by default. Vue does something clever by only applying hidden at the end I think.

--- resources/js/Components/Dropdown.svelte
+++ resources/js/Components/Dropdown.svelte
@@ -1,5 +1,7 @@
 <script setup lang="ts">
     import { onMount } from 'svelte';
+    import { scale } from 'svelte/transition';
+    import { cubicIn, cubicOut } from 'svelte/easing';
 
     export let align: 'left' | 'right' = 'right';
     // TODO: ... width as string :what:
@@ -46,26 +48,21 @@
     </div>
 
     <!-- Full Screen Dropdown Overlay -->
-
     <div hidden={!open} class="fixed inset-0 z-40" on:click={() => (open = false)}></div>
 
-    <!-- <Transition
-            enter-active-class="transition ease-out duration-200"
-            enter-from-class="opacity-0 scale-95"
-            enter-to-class="opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-75"
-            leave-from-class="opacity-100 scale-100"
-            leave-to-class="opacity-0 scale-95"
-        > -->
-    <div
-        class="absolute z-50 mt-2 rounded-md shadow-lg {widthClass} {alignmentClasses}"
-        hidden={!open}
-        on:click={() => (open = false)}
-    >
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {contentClasses}">
-            <slot name="content" />
+    {#if open}
+        <div
+            in:scale={{ start: 0.95, opacity: 0, duration: 200, easing: cubicIn }}
+            out:scale={{ start: 0.95, opacity: 0, duration: 75, easing: cubicOut }}
+        >
+            <div
+                class="absolute z-50 mt-2 rounded-md shadow-lg {widthClass} {alignmentClasses}"
+                on:click={() => (open = false)}
+            >
+                <div class="rounded-md ring-1 ring-black ring-opacity-5 {contentClasses}">
+                    <slot name="content" />
+                </div>
+            </div>
         </div>
-    </div>
-
-    <!-- </Transition> -->
+    {/if}
 </div>
