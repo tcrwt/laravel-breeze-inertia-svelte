f423d7e6c67d09998c127da4d0e48e8fe761a277
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-20 20:39:59 +0100

    disable ssr because it doesn't work

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,16 +1,21 @@
-import { createInertiaApp } from '@inertiajs/svelte';
-import createServer from '@inertiajs/svelte/server';
+// Doesn't work – see
+// https://github.com/inertiajs/inertia/issues/1760
+// Possibly fixed in 
+// https://github.com/inertiajs/inertia/pull/1866
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// import { createInertiaApp } from '@inertiajs/svelte';
+// import createServer from '@inertiajs/svelte/server';
 
-createServer((page) =>
-    createInertiaApp({
-        page,
-        title: (title) => `${title} - ${appName}`,
-        resolve: (name) => {
-            const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true });
-            return pages[`./Pages/${name}.svelte`];
-        },
-    }),
-);
+// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+// createServer((page) =>
+//     createInertiaApp({
+//         page,
+//         title: (title) => `${title} - ${appName}`,
+//         resolve: (name) => {
+//             const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true });
+//             return pages[`./Pages/${name}.svelte`];
+//         },
+//     }),
+// );
 
