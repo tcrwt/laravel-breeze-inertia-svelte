cc8a401737a6fe15ece83fa183b09813f644dcb4
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-20 06:44:21 +0100

    Create Portal.svelte

---
+++ resources/js/Components/Portal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    // https://github.com/sveltejs/svelte/issues/3088#issuecomment-505785516
+    import { onMount, onDestroy } from 'svelte';
+    let ref: HTMLDivElement;
+    let portal: HTMLElement;
+
+    onMount(() => {
+        portal = document.createElement('div');
+        portal.className = 'portal';
+        document.body.appendChild(portal);
+        portal.appendChild(ref);
+    });
+
+    onDestroy(() => {
+        document.body.removeChild(portal);
+    });
+</script>
+
+<div class="hidden">
+    <div bind:this={ref}>
+        <slot />
+    </div>
+</div>
