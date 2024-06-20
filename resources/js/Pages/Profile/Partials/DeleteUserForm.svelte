c779c548dee4684afbd2881c100ca2ce65abc1a9
Tim Crowter <tim.crowter@sparx.co.uk>
2024-06-20 20:18:59 +0100

    format

--- resources/js/Pages/Profile/Partials/DeleteUserForm.svelte
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.svelte
@@ -1,71 +1,91 @@
-<script setup lang="ts">
-import DangerButton from '@/Components/DangerButton.svelte';
-import InputError from '@/Components/InputError.svelte';
-import InputLabel from '@/Components/InputLabel.svelte';
-import Modal from '@/Components/Modal.svelte';
-import SecondaryButton from '@/Components/SecondaryButton.svelte';
-import TextInput from '@/Components/TextInput.svelte';
-import { useForm } from '@inertiajs/svelte';
+<script lang="ts">
+    import DangerButton from '@/Components/DangerButton.svelte';
+    import InputError from '@/Components/InputError.svelte';
+    import InputLabel from '@/Components/InputLabel.svelte';
+    import Modal from '@/Components/Modal.svelte';
+    import SecondaryButton from '@/Components/SecondaryButton.svelte';
+    import TextInput from '@/Components/TextInput.svelte';
+    import { useForm } from '@inertiajs/svelte';
+    import { onMount, tick } from 'svelte';
 
-const confirmingUserDeletion = ref(false);
-const passwordInput = ref<HTMLInputElement | null>(null);
+    let confirmingUserDeletion = false;
+    let passwordInput: TextInput;
 
-const form = useForm({
-    password: '',
-});
 
-const confirmUserDeletion = () => {
-    confirmingUserDeletion.value = true;
-
-    nextTick(() => passwordInput.value?.focus());
-};
-
-const deleteUser = () => {
-    $form.delete(route('profile.destroy'), {
-        preserveScroll: true,
-        onSuccess: () => closeModal(),
-        onError: () => passwordInput.value?.focus(),
-        onFinish: () => {
-            $form.reset();
-        },
+    const form = useForm({
+        password: '',
     });
-};
 
-const onKeyUp = (event: KeyboardEvent) => {
-    if (event.key === 'Enter') {
-        deleteUser();
+    const confirmUserDeletion = async () => {
+        confirmingUserDeletion = true;
+        await tick();
+        passwordInput?.focus();
+    };
+
+    const deleteUser = () => {
+        $form.delete(route('profile.destroy'), {
+            preserveScroll: true,
+            onSuccess: () => {
+                console.log('onSuccess');
+                closeModal();
+            },
+            onError: () => {
+                console.log('onError');
+                passwordInput?.focus();
+            },
+            onFinish: () => {
+                console.log('onFinish');
+                $form.reset();
+            },
+        });
+    };
+
+    const onKeyUp = (event: any) => {
+        if (event.key === 'Enter') {
+            deleteUser();
+        }
+    };
+
+    onMount(() => {
+        document.addEventListener('keyup', onKeyUp);
+
+        return () => {
+            document.removeEventListener('keyup', onKeyUp);
+        };
+    });
+    $: {
+        console.log({ confirmingUserDeletion });
     }
-};
+    const closeModal = () => {
+        confirmingUserDeletion = false;
 
-const closeModal = () => {
-    confirmingUserDeletion.value = false;
-
-    $form.reset();
-};
+        $form.reset();
+    };
 </script>
 
-
-<section class="space-y-6">
+<section {...$$restProps} class="{$$props.class || ''} space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900">Delete Account</h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting
-            your account, please download any data or information that you wish to retain.
+            Once your account is deleted, all of its resources and data will be
+            permanently deleted. Before deleting your account, please download any data or
+            information that you wish to retain.
         </p>
     </header>
 
-    <DangerButton on:click={confirmUserDeletion}>Delete Account</DangerButton>
+    <DangerButton on:click={() => confirmUserDeletion()}>Delete Account</DangerButton>
 
-    <Modal hidden={!confirmingUserDeletion} @close="closeModal">
+    <Modal show={confirmingUserDeletion} {closeModal}>
         <div class="p-6">
             <h2 class="text-lg font-medium text-gray-900">
                 Are you sure you want to delete your account?
             </h2>
 
             <p class="mt-1 text-sm text-gray-600">
-                Once your account is deleted, all of its resources and data will be permanently deleted. Please
-                enter your password to confirm you would like to permanently delete your account.
+                Once your account is deleted, all of its resources and data will be
+                permanently deleted. Please enter your password to confirm you would like
+                to permanently delete your account.
             </p>
 
             <div class="mt-6">
@@ -73,8 +93,8 @@ const closeModal = () => {
 
                 <TextInput
                     id="password"
-                    ref="passwordInput"
-                    bind:input={$form.password}
+                    bind:this={passwordInput}
+                    bind:value={$form.password}
                     type="password"
                     class="mt-1 block w-3/4"
                     placeholder="Password"
@@ -85,13 +105,12 @@ const closeModal = () => {
             </div>
 
             <div class="mt-6 flex justify-end">
-                <SecondaryButton @click="closeModal"> Cancel </SecondaryButton>
+                <SecondaryButton on:click={closeModal}>Cancel</SecondaryButton>
 
                 <DangerButton
-                    class="ms-3"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                    @click="deleteUser"
+                    class="ms-3 {$form.processing ? 'opacity-25' : ''}"
+                    disabled={$form.processing}
+                    on:click={deleteUser}
                 >
                     Delete Account
                 </DangerButton>
